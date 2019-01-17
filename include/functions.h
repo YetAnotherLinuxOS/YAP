035d722478164ccefeebf5166b93dff8a7e84e4a
UserUnavailable <klebersilvanaruto6@gmail.com>
2019-01-16 22:18:31 -0200

    removed using namespace

--- include/functions.h
+++ include/functions.h
@@ -6,10 +6,9 @@
 #include <experimental/filesystem>
 #include "cpptoml.h"
 
-using namespace std;
 namespace fs = std::experimental::filesystem;
 
-vector<string> getfile(string rex);
-void info(string name);
+std::vector<std::string> getfile(std::string rex);
+void info(std::string name);
 
 #endif
