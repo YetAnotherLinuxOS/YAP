49682e109e3497d4b22dad1adb2dc3079e4b80e6
UserUnavailable <klebersilvanaruto6@gmail.com>
2019-01-17 16:57:03 -0200

    functions

--- src/functions.cpp
+++ src/functions.cpp
@@ -1,33 +1,11 @@
-#include "../include/functions.h"
+#include "../include/yap.h"
 
-// function that search for REGEX filename in directory
-std::vector<std::string> getfile(std::string rex) {
-    fs::path ybh_path = fs::current_path(); // directory to read
-
-    // regex; variable rex plus regex /.*/
-    // smatch; used in 'regex_match' to list /matches/
-    std::regex reg(rex+".*");
-    std::smatch matches;
-
-    // create a string vector and list the files name in directory
-    std::vector<std::string> ybhs;
-    for (const auto& dirname : fs::directory_iterator(ybh_path)) {
-        if (fs::is_regular_file(dirname)) ybhs.push_back(fs::path(dirname).filename());
-    }
-
-    // create string vector with the matches
-    std::vector<std::string> returning;
-    for (const auto& ybh : ybhs) {
-        if (std::regex_match(ybh, matches, reg)) returning.push_back(matches[0]);
-    }
-
-    return returning;
-}
+namespace yts = yap::toml_string;
 
 // function that read a file.toml and return info
 void info(std::string name) {
     // if more than 1 pkgs is found; return a list with it
-    std::vector<std::string> pkgs = getfile(name);
+    std::vector<std::string> pkgs = yap::getfile(name);
     if (pkgs.size() > 1) {
         for (const auto& pkg : pkgs) std::cout << pkg << std::endl;
         return;
@@ -36,32 +14,28 @@ void info(std::string name) {
         return;
     }
 
-    // initialize file.toml
-    auto config = cpptoml::parse_file(pkgs[0]);
-
-    // get info from [info] table in file.toml
-    auto info = config->get_table("info");
-    auto p_name = info->get_as<std::string>("name");
-    auto p_ver = info->get_as<std::string>("version");
-    auto p_desc = info->get_as<std::string>("description");
-    auto p_man = info->get_as<std::string>("manpage");
-    auto p_lis = info->get_as<std::string>("license");
+    // get strings from 'info' table in file.toml 
+    std::string p_name = yts::table(pkgs[0], "name"       , "info");
+    std::string p_ver  = yts::table(pkgs[0], "version"    , "info");
+    std::string p_desc = yts::table(pkgs[0], "description", "info");
+    std::string p_man  = yts::table(pkgs[0], "manpage"    , "info");
+    std::string p_lis  = yts::table(pkgs[0], "license"    , "info");
 
     // get array from 'dependences' key in file.toml
-    auto deps = config->get_array_of<std::string>("dependences");
-
+    std::vector<std::string> deps = yts::array(pkgs[0], "dependences");
+    
     // output info from file.toml
-    std::string infout = "Info About: " + (*p_name) +"-"+ (*p_ver) + "\n\n"
-                        "name         :\t" + (*p_name) + "\n"
-                        "version      :\t" + (*p_ver)  + "\n"
-                        "description  :\t" + (*p_desc) + "\n"
-                        "manpage      :\t" + (*p_man)  + "\n"
-                        "license      :\t" + (*p_lis)  + "\n";
+    std::string infout = "Info About: "     + p_name + "-" + p_ver + "\n\n"
+                         "name         :\t" + p_name + "\n"
+                         "version      :\t" + p_ver  + "\n"
+                         "description  :\t" + p_desc + "\n"
+                         "manpage      :\t" + p_man  + "\n"
+                         "license      :\t" + p_lis  + "\n";
 
     std::cout << infout << std::endl;
     
     // output dependence info from file.toml
     std::cout << "Dependences:\n" << std::endl;
-    for (const auto& dep : *deps) std::cout << "\t" << dep << std::endl;
+    for (const auto& dep : deps) std::cout << "\t" << dep << std::endl;
     std::cout << std::endl;
 }
