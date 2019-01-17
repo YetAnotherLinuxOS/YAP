131bad91864267a8131ac355c044ae4595879b1b
UserUnavailable <klebersilvanaruto6@gmail.com>
2019-01-17 16:56:27 -0200

    added utils

---
+++ include/utils.h
@@ -0,0 +1,13 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+#include <iostream>
+#include <regex>
+#include <experimental/filesystem>
+#include "cpptoml.h"
+
+namespace fs = std::experimental::filesystem;
+
+void info(std::string name);
+
+#endif
