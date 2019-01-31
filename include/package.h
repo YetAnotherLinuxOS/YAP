aa2ab6c80a7ba9b362f346e858fa25f56c12dd52
Anaboth Hekmatyar <anaboth.hek@gmail.com>
2019-01-31 13:40:34 -0200

    Reformated all the code

--- include/package.h
+++ include/package.h
@@ -1,30 +1,31 @@
 #ifndef PACKAGE
 #define PACKAGE
 
-#include <string>
 #include "cpptoml.h"
 #include "yap.h"
+#include <string>
 
 namespace yap {
-    class Package {
-    private:
-        std::string name;
-        std::string version;
-        std::string license;
-        std::string manpage;
-        std::string description;
-        std::string source_link;
-        std::vector<std::string> dependecies;
-        std::vector<std::string> compile_make;
-        std::vector<std::string> compile_install;
-        std::vector<std::string> compile_uninstall;
-        std::vector<std::string> patches;
-    public:
-        std::string get_info();
-        Package(std::string);
-        void Download();
-        void Compile();
-    };
-}
+class Package {
+private:
+  std::string name;
+  std::string version;
+  std::string license;
+  std::string manpage;
+  std::string description;
+  std::string source_link;
+  std::vector<std::string> dependecies;
+  std::vector<std::string> compile_make;
+  std::vector<std::string> compile_install;
+  std::vector<std::string> compile_uninstall;
+  std::vector<std::string> patches;
+
+public:
+  std::string get_info();
+  Package(std::string);
+  void Download();
+  void Compile();
+};
+} // namespace yap
 
 #endif // PACKAGE
--- include/yap.h
+++ include/yap.h
@@ -2,55 +2,57 @@
 #define YAP_H
 
 #include "cpptoml.h"
-#include <regex>
-#include <iostream>
-#include <experimental/filesystem>
 #include <cstdlib>
-#include <unistd.h>
 #include <errno.h>
+#include <experimental/filesystem>
+#include <iostream>
+#include <regex>
 #include <sys/wait.h>
+#include <unistd.h>
 
 namespace fs = std::experimental::filesystem;
 
-char** vec_to_array(std::vector<std::string>);
-void free_array(char**);
+char **vec_to_array(std::vector<std::string>);
+void free_array(char **);
 std::vector<std::string> split(std::string, std::string = " ");
 
 namespace yap {
-    // vector<string> getfile(string _regex_);
-    std::vector<std::string> getfile(std::string);
-    
-    // int launcher(vector<string> _commands_);
-    int launcher(std::vector<std::string>);
+// vector<string> getfile(string _regex_);
+std::vector<std::string> getfile(std::string);
 
-    // int launcher(const char _*command[]_);
-    int launcher(const char** command); /* only for legacy purposes */
-    
-    // void download(string _sourceLink_, string _name_);
-    void download(std::string, std::string);
-    
-    // void extract(string _filename.tar.gz_);
-    void extract(std::string, std::string);
-    
-    // void compile(string _sourceLink_, string _name_);
-    void compile(std::string, std::string, std::vector<std::string>, std::vector<std::string>, std::vector<std::string>);
+// int launcher(vector<string> _commands_);
+int launcher(std::vector<std::string>);
 
-    // void run(vector<string> _commands_);
-    void run(std::vector<std::string>);
+// int launcher(const char _*command[]_);
+int launcher(const char **command); /* only for legacy purposes */
 
-    // void apply_patches(vector<string> _patches_);
-    void apply_patches(std::vector<std::string>);
+// void download(string _sourceLink_, string _name_);
+void download(std::string, std::string);
 
-    namespace toml_string {
-        // string table(string _filename_, string _keyname_, string _tablename_);
-        std::string table(std::string, std::string, std::string);
-        
-        // vector<string> array(string _filename_, string _keyname_);
-        std::vector<std::string> array(std::string, std::string);
-        
-        //  vector<string> tarray(string _filename_, string _keyname_, string _tablename_);
-        std::vector<std::string> tarray(std::string, std::string, std::string);
-    }
-}
+// void extract(string _filename.tar.gz_);
+void extract(std::string, std::string);
+
+// void compile(string _sourceLink_, string _name_);
+void compile(std::string, std::string, std::vector<std::string>,
+             std::vector<std::string>, std::vector<std::string>);
+
+// void run(vector<string> _commands_);
+void run(std::vector<std::string>);
+
+// void apply_patches(vector<string> _patches_);
+void apply_patches(std::vector<std::string>);
+
+namespace toml_string {
+// string table(string _filename_, string _keyname_, string _tablename_);
+std::string table(std::string, std::string, std::string);
+
+// vector<string> array(string _filename_, string _keyname_);
+std::vector<std::string> array(std::string, std::string);
+
+//  vector<string> tarray(string _filename_, string _keyname_, string
+//  _tablename_);
+std::vector<std::string> tarray(std::string, std::string, std::string);
+} // namespace toml_string
+} // namespace yap
 
 #endif
