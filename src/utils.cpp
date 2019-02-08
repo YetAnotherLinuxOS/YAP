42e8c379541192e30ca1b374b487ab92bc18e4ff
UserUnavailable <klebersilva2025@gmail.com>
2019-02-08 18:08:36 -0200

    removed getfile()

--- src/utils.cpp
+++ src/utils.cpp
@@ -13,38 +13,6 @@ std::string yap::get_ybh(std::string name) {
   return std::max_element(versions.begin(), versions.end())->c_str();
 }
 
-// function that search for REGEX filename in directory
-std::vector<std::string> yap::getfile(std::string rex) {
-  fs::path ybh_path = "test/"; // directory to read; '/' on the end IS necessary
-
-  // regex; variable rex plus regex /.*/
-  // smatch; used in 'regex_match' to list /matches/
-  std::regex reg(rex + ".*");
-  std::smatch matches;
-
-  // create a string vector and list the files name in directory
-  std::vector<std::string> ybhs;
-  for (const auto &dirname : fs::directory_iterator(ybh_path)) {
-    if (fs::is_regular_file(dirname))
-      ybhs.push_back(fs::path(dirname).filename());
-  }
-
-  // create string vector with the matches
-  std::vector<std::string> returning;
-  for (const auto &ybh : ybhs) {
-    if (std::regex_match(ybh, matches, reg))
-      returning.push_back(matches[0]);
-  }
-
-  // if returning have only one element
-  // insert ybh_path to the begin of the string
-  if (returning.size() == 1) {
-    returning.at(0).insert(0, std::string(ybh_path));
-  }
-
-  return returning;
-}
-
 // exec system commands - legacy
 int yap::launcher(const char **command) {
   if (!fork()) {
